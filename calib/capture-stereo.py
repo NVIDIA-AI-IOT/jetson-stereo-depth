5653888fd5939cfa48d92637e8201cc0a8bb9252
Tomasz Lewicki <t.w.lewicki@gmail.com>
2021-10-08 01:15:41 -0700

    Update calibration notebooks

--- calib/capture-stereo.py
+++ calib/capture-stereo.py
@@ -1,26 +1,39 @@
 import time
 import cv2
+import sys
+import numpy as np
 
+sys.path.append("..")
 from camera import Camera
 
 if __name__ == "__main__":
     
-    cam_l= Camera(0, flip=True)
-    cam_r = Camera(1, flip=True)
+    cam_l= Camera(0)
+    cam_r = Camera(1)
     cnt = 0
 
-    while True:
+    try:
+        while True:
 
-        img_l = cam_l.read()
-        img_r = cam_r.read()
-        
-        print(img_l)
-        
-        cv2.imshow("windowname", img_l)
-        key = cv2.waitKey(1) & 0xFF
+            print("Capturing. Press spacebar to capture an image pair.%%")
 
-        if key == ord(" "):
-            cnt +=1
-            cv2.imwrite(f"calib_pairs/right/{cnt:03d}.png", img_r)
-            cv2.imwrite(f"calib_pairs/left/{cnt:03d}.png", img_l)
-            print("pic!")
+            img_l = cam_l.read()
+            img_r = cam_r.read()
+
+            img_lr = np.hstack([img_l, img_r])
+            img_lr = cv2.resize(img_lr, (1920, 1080//2))
+            
+            cv2.imshow("windowname", img_lr)
+
+            key = cv2.waitKey(1) & 0xFF
+
+            if key == ord(" "):
+                cnt +=1
+                cv2.imwrite(f"/home/nx/jetson-stereo-depth/calib/calib_images2/right/{cnt:03d}.png", img_r)
+                cv2.imwrite(f"/home/nx/jetson-stereo-depth/calib/calib_images2/left/{cnt:03d}.png", img_l)
+                print("pic!")
+    except KeyboardInterrupt as e:
+        print("closing")
+    finally:
+        cam_l.stop()
+        cam_r.stop()
